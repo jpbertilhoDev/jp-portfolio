4ba16f5f277b6aeea6bf2d8dfa5a268fc0663b6f
Pedro Calixtrato <E3158@itsector.pt>
2024-03-19 10:06:37 +0000

    Inicializando portfolio

---
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+//Components
+import { HeaderComponent } from '../../components/header/header.component';
+import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent, KnowledgeComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
